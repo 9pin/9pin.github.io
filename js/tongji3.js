c92a5f12af17fc7785db8da389467ef8f16630a6
9pin <40909660+9pin@users.noreply.github.com>
2019-04-11 07:14:09 +0800

    Add files via upload

---
+++ js/tongji3.js
@@ -0,0 +1,11 @@
+/** CNZZ统计 **
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1254444291'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/stat.php%3Fid%3D1254444291' type='text/javascript'%3E%3C/script%3E"));
+/** 百度统计 **/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?56f0024bdc18f18fb04dd08a8b412404";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
